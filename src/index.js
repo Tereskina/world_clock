f13ca57b79b48db53d6a7de1a6872481c1f869d2
Kseniia Tereskina <39565570+Tereskina@users.noreply.github.com>
2024-03-31 21:55:02 +0100

    set time for Tokyo and Paris

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function updateTokyoTime() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("d:mm:ss [<small>]A[</small>]");
+}
+
+function updateParisTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+setInterval(updateTokyoTime, 1000);
+setInterval(updateParisTime, 1000);
